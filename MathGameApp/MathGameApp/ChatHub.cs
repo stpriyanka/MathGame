95bdf6d864e9b1ff9cc7a019a446212f339612fd
Priyanka S.Tasnia <stpriyanka2011@gmail.com>
2017-11-13 23:23:45 +0100

    implemented SignalR

---
+++ MathGameApp/MathGameApp/ChatHub.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MathGameApp
+{
+	public class ChatHub : Hub
+	{
+		public void Send(string name)
+		{
+			var u = new Users()
+			{
+				ConnectionId = Context.ConnectionId,
+				UserName = name
+			};
+			CurrentConnections.Add(u);
+
+			// Call the broadcastMessage method to update clients.
+			Clients.All.broadcastMessage(name);
+		}
+
+		readonly List<Users> CurrentConnections = new List<Users>();
+
+		public override Task OnConnected()
+		{
+			return base.OnConnected();
+		}
+
+		public override Task OnDisconnected(bool stopCalled)
+		{
+			//var connection = CurrentConnections.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+
+			//if (connection != null)
+			//{
+			//	CurrentConnections.Remove(connection);
+			//}
+
+			return base.OnDisconnected(true);
+		}
+	}
+
+	public class Users
+	{
+		public string ConnectionId { get; set; }
+		public string UserName { get; set; }
+	}
+}
